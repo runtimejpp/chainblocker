a4dc7c84233d493848132d290344776dad9d3ea9
Jason Preneveau <jpnamausa@gmail.com>
2022-08-11 02:52:27 -0400

    modified:   dev/api.js 	modified:   package-lock.json 	modified:   package.json "Need comments"

--- dev/api.js
+++ dev/api.js
@@ -1,19 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
 
 // Creating the API end point for the blockchain
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended:false
+}));
 
-app.get("/blockchain", function (req, res) {
-   
+
+app.get("/blockchain", function(req, res) {
+  console.log(req, body);
+
+  res.send("blockchain API Working properly");
 });
 
 // API  endpoint for transactions
-app.post("/transaction", function (req, res) {});
+app.post("/transaction", function(req, res) {
+  console.log("transactions");
+  res.send(`The amout of the transaction is ${req.body.amout} bitcoin.`);
+});
 
 // API where new block creation takes place - mining
-app.get("/mine", function (req, res) {});
+app.get("/mine", function(req, res) {
+  //  console.log("miner");
+  // res.send("Mining API endpoint working properly");
+});
 
 // We will know that the server is running when we see the Listening on port 3000...
-app.listen(3000, function () {
-    console.log("Listeining on port 3000...");
+app.listen(3000, function() {
+  console.log("Listing on port 3000...");
 });
